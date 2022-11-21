459027a048e04283fec3f2b3c3ff85a9e72328ea
kkukileon305 <kkukileon305@gmail.com>
2022-11-22 00:07:42 +0900

    useInfiniteQuery와 intersection Observer로 무한 스크롤 구현

--- src/components/board/react-query/BoardList.tsx
+++ src/components/board/react-query/BoardList.tsx
@@ -1,17 +1,53 @@
-import { useQuery } from '@tanstack/react-query';
-import { Post, PostsResponse } from '../../../interface';
+import { useEffect, useRef, useState } from 'react';
+import { ImSpinner2 } from 'react-icons/im';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { PostsResponse } from '../../../interface';
 import axiosInstance from '../../../utils/axiosInstance';
 import PostItem from '../PostItem';
 
 const BoardList = () => {
-  const { data: posts } = useQuery<Post[]>({
-    queryFn: () => axiosInstance.get<PostsResponse>('/board').then(res => res.data.posts),
+  const { data: postsData, fetchNextPage } = useInfiniteQuery({
     queryKey: ['getPosts'],
+    queryFn: ({ pageParam = 1 }) => axiosInstance.get<PostsResponse>(`/board?page=${pageParam}`).then(res => res.data.posts),
+    getNextPageParam: (_, allPage) => allPage.length * 8 + 1,
   });
+
+  const spinnerRef = useRef<HTMLDivElement>(null);
+  const [isEnd, setIsEnd] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting && entry.intersectionRatio > 0) {
+            fetchNextPage();
+          }
+        });
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    spinnerRef.current && observer.observe(spinnerRef.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    if (postsData && postsData.pages.slice(-1)[0].length < 8) {
+      setIsEnd(true);
+    }
+  }, [postsData]);
 
   return (
     <div className='py-4'>
-      <ul className='border flex flex-col-reverse'>{posts ? posts.map(post => <PostItem post={post} key={post.id} />) : <h2>불러오는중...</h2>}</ul>
+      <ul className='border'>{postsData?.pages.map(posts => posts.map(post => <PostItem post={post} key={post.id} />))}</ul>
+      {!isEnd && (
+        <div ref={spinnerRef} className='flex justify-center py-4'>
+          <ImSpinner2 className='animate-spin' size={40} />
+        </div>
+      )}
     </div>
   );
 };
